883c7b6265d32754601836119e969e4a929c9487
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-07 13:05:19 +0400

    .

--- 0x01/numbers/Numbers0to99.java
+++ 0x01/numbers/Numbers0to99.java
@@ -3,8 +3,8 @@ package numbers;
 public class Numbers0to99 {
     public static void main(String[] args) {
         for(int i = 0; i<100; i++){
-            System.out.printf(i+ ", ");
+            System.out.print(i+ ", ");
         }
-
+        System.out.println();
     }
 }
