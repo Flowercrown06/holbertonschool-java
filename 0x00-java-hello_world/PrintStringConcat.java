287630810c9db37cd496762a010047226053d721
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-07 12:09:39 +0400

    .

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,8 +1,8 @@
 public class PrintStringConcat {
     public static void main(String[] args) {
         String text1= "Holberton";
-        String text2= " School";
-        String result= "Welcome to " + text1.concat(text2)+ " !";
+        String text2= "School";
+        String result= "Welcome to " + text1.concat(" ").concat(text2)+ " !";
         System.out.println(result);
     }
 }
