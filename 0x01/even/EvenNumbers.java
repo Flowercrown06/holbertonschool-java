4314784bcb8994611e9e7408525c7122ce7bd8bf
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-07 21:52:02 +0400

    q

--- 0x01/even/EvenNumbers.java
+++ 0x01/even/EvenNumbers.java
@@ -3,7 +3,11 @@ public class EvenNumbers {
     public static void main(String[] args) {
         for(int i = 0 ; i<100; i++){
             if(i%2==0){
-                System.out.print(i+ " ");
+                if (i < 98) {
+                    System.out.print(i + ", ");
+                } else {
+                    System.out.print(i);
+                }
             }
         }
     }
