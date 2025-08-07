70cb08cd72a47662781166c153d7d58f865dde6f
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-07 22:06:22 +0400

    a

--- 0x01/uppercase/Program.java
+++ 0x01/uppercase/Program.java
@@ -3,11 +3,11 @@ package uppercase;
 public class Program {
     public static void main(String[] args) {
 
-        boolean characterInUppercase1 = Character.IsUpper('a');
-        boolean characterInUppercase2 = Character.IsUpper('B');
-        boolean characterInUppercase3 = Character.IsUpper('d');
-        boolean characterInUppercase4 = Character.IsUpper('f');
-        boolean characterInUppercase5 = Character.IsUpper('G');
+        boolean characterInUppercase1 = Character.isUpper('a');
+        boolean characterInUppercase2 = Character.isUpper('B');
+        boolean characterInUppercase3 = Character.isUpper('d');
+        boolean characterInUppercase4 = Character.isUpper('f');
+        boolean characterInUppercase5 = Character.isUpper('G');
 
         System.out.printf("%b\n", characterInUppercase1);
         System.out.printf("%b\n", characterInUppercase2);
