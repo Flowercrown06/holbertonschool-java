1c43d59a098fb15a2ab81deec8bb6c9be74d7476
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-11 17:00:10 +0400

    Done

--- 0x01/uppercase/Program.java
+++ 0x01/uppercase/Program.java
@@ -1,11 +1,12 @@
+
 public class Program {
     public static void main(String[] args) {
 
-        boolean characterInUppercase1 = Character.isUpperCase('a');
-        boolean characterInUppercase2 = Character.isUpperCase('B');
-        boolean characterInUppercase3 = Character.isUpperCase('d');
-        boolean characterInUppercase4 = Character.isUpperCase('f');
-        boolean characterInUppercase5 = Character.isUpperCase('G');
+        boolean characterInUppercase1 = Character.IsUpper('a');
+        boolean characterInUppercase2 = Character.IsUpper('B');
+        boolean characterInUppercase3 = Character.IsUpper('d');
+        boolean characterInUppercase4 = Character.IsUpper('f');
+        boolean characterInUppercase5 = Character.IsUpper('G');
 
         System.out.printf("%b\n", characterInUppercase1);
         System.out.printf("%b\n", characterInUppercase2);
