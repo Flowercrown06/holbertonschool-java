5590ca4b5a99474ff149581d624be9565b89e8fd
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-14 00:51:20 +0400

    done

--- 0x02/square/Square.java
+++ 0x02/square/Square.java
@@ -2,7 +2,7 @@
 public class Square {
     public static double area(double side){
         if (side > 0) {
-            return side*4;
+            return side*side;
         } throw new IllegalArgumentException("Side must have a positive value.");
     }
 }
