880410c93c604258365545b04e46bd4c2cba588e
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-14 01:02:45 +0400

    done

--- 0x03/salary/Program.java
+++ 0x03/salary/Program.java
@@ -1,4 +1,5 @@
 public class Program {
+
     public static void main(String[] args) {
         Department engineeringDepartment = new Department(1000, 1100);
         Department documentationDepartment = new Department(1000, 800);
@@ -11,34 +12,26 @@ public class Program {
         Employee documentationEmployee2 = new Employee(3000);
         Manager documentationManager = new Manager(4980);
 
-        System.out.printf("Fixed Salary: %.2f - Total Salary: %.2f - Bonus Amount: %.2f\n",
-                engineeringEmployee1.getFixedSalary(),
-                engineeringEmployee1.calculateTotalSalary(engineeringDepartment),
-                engineeringEmployee1.calculateBonus(engineeringDepartment));
+        printBonuses(engineeringEmployee1, engineeringDepartment);
+        printBonuses(engineeringEmployee2, engineeringDepartment);
+        printBonuses(engineeringManager, engineeringDepartment);
+        printBonuses(documentationEmployee1, documentationDepartment);
+        printBonuses(documentationEmployee2, documentationDepartment);
+        printBonuses(documentationManager, documentationDepartment);
+    }
 
-        System.out.printf("Fixed Salary: %.2f - Total Salary: %.2f - Bonus Amount: %.2f\n",
-                engineeringEmployee2.getFixedSalary(),
-                engineeringEmployee2.calculateTotalSalary(engineeringDepartment),
-                engineeringEmployee2.calculateBonus(engineeringDepartment));
+    private static void printBonuses(Employee employee, Department department) {
+        String fixedSalary = Double.toString(employee.getFixedSalary());
+        String totalSalary = Double.toString(
+                employee.calculateTotalSalary(department)
+        );
+        String bonus = Double.toString(employee.calculateBonus(department));
 
-        System.out.printf("Fixed Salary: %.2f - Total Salary: %.2f - Bonus Amount: %.2f\n",
-                engineeringManager.getFixedSalary(),
-                engineeringManager.calculateTotalSalary(engineeringDepartment),
-                engineeringManager.calculateBonus(engineeringDepartment));
-
-        System.out.printf("Fixed Salary: %.2f - Total Salary: %.2f - Bonus Amount: %.2f\n",
-                documentationEmployee1.getFixedSalary(),
-                documentationEmployee1.calculateTotalSalary(documentationDepartment),
-                documentationEmployee1.calculateBonus(documentationDepartment));
-
-        System.out.printf("Fixed Salary: %.2f - Total Salary: %.2f - Bonus Amount: %.2f\n",
-                documentationEmployee2.getFixedSalary(),
-                documentationEmployee2.calculateTotalSalary(documentationDepartment),
-                documentationEmployee2.calculateBonus(documentationDepartment));
-
-        System.out.printf("Fixed Salary: %.2f - Total Salary: %.2f - Bonus Amount: %.2f\n",
-                documentationManager.getFixedSalary(),
-                documentationManager.calculateTotalSalary(documentationDepartment),
-                documentationManager.calculateBonus(documentationDepartment));
+        System.out.printf(
+                "Fixed Salary: %s,%s - Total Salary: %s,%s - Bonus Amount: %s,%s\n",
+                fixedSalary.split("[.]")[0], fixedSalary.split("[.]")[1].length() == 1 ? fixedSalary.split("[.]")[1] + "0" : fixedSalary.split("[.]")[1],
+                totalSalary.split("[.]")[0], totalSalary.split("[.]")[1].length() == 1 ? totalSalary.split("[.]")[1] + "0" : totalSalary.split("[.]")[1],
+                bonus.split("[.]")[0], bonus.split("[.]")[1].length() == 1 ? bonus.split("[.]")[1] + "0" : bonus.split("[.]")[1]
+        );
     }
 }
