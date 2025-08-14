e73cc4ba3176150d8eade5200f7ea869465dd729
HUAWEI <gultacquliyeva26@gmail.com>
2025-08-14 13:09:46 +0400

    done

--- Classes_Objects/character/CharacterGame.java
+++ Classes_Objects/character/CharacterGame.java
@@ -1,4 +1,4 @@
-package character;
+
 
 public class CharacterGame {
     public int currentHealth;
--- Classes_Objects/character/Program.java
+++ Classes_Objects/character/Program.java
@@ -1,4 +1,4 @@
-package character;
+
 
 public class Program {
     public static void main(String[] args) {
