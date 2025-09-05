e57cb5a65d3c9735fc6552862359c114549fbce6
HUAWEI <gultacquliyeva26@gmail.com>
2025-09-06 01:10:08 +0400

    done

--- PrimitiveTypes_Wrapper/positive_integer/PositiveInteger.java
+++ PrimitiveTypes_Wrapper/positive_integer/PositiveInteger.java
@@ -1,55 +1,36 @@
-
-// PositiveInteger.java
 public class PositiveInteger {
 
     private int value;
-
-    // Constructor int ilə
-    public PositiveInteger(int value) {
-        if (value <= 0) {
-            throw new IllegalArgumentException("Value is not a positive integer");
-        }
-        this.value = value;
-    }
-
-    // Constructor String ilə
-    public PositiveInteger(String strValue) {
-        try {
-            int intValue = Integer.parseInt(strValue);
-            if (intValue <= 0) {
-                throw new IllegalArgumentException("Value is not a positive integer");
-            }
-            this.value = intValue;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Value is not a positive integer");
-        }
-    }
 
     public int getValue() {
         return value;
     }
 
-    // Dəyəri dəyişmək üçün metod
-    public void setValue(int value) {
-        if (value <= 0) {
-            throw new IllegalArgumentException("Value is not a positive integer");
-        }
+    public void setValue (int value) throws IllegalArgumentException {
+        if (value < 0) throw new IllegalArgumentException("Value is not a positive integer");
         this.value = value;
     }
 
-    // Prime olub olmadığını yoxlayan metod
+    public void setValor (int value) throws IllegalArgumentException {
+        if (value < 0) throw new IllegalArgumentException("Value is not a positive integer");
+        this.value = value;
+    }
+
+    public PositiveInteger(int value) throws IllegalArgumentException {
+        setValue(Integer.valueOf(value));
+    }
+
+    public PositiveInteger(String value) throws IllegalArgumentException{
+        setValue(Integer.valueOf(value));
+    }
+
+
     public boolean isPrime() {
         if (value <= 1) return false;
-        if (value == 2) return true;
 
         for (int i = 2; i <= Math.sqrt(value); i++) {
             if (value % i == 0) return false;
         }
         return true;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(value);
     }
 }
