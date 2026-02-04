e895e6f6d99ffd3c1a30f135a892570e48087387
HUAWEI <gultacquliyeva26@gmail.com>
2026-02-04 18:40:02 +0400

    done

--- 0x00/name/Name.java
+++ 0x00/name/Name.java
@@ -1,38 +1,39 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
 public class Name {
-    // Self-explanatory variables
-    // A
+
+    // Self-explanatory variables (choose A)
     public Date modifiedDate;
-    public List<Testcase> findAllTestcasesByUser(Name.User user){};
-    // B
-    // Correct Declarations
-    // A
-    public List<Account> AccountList;
-    // B
+    public List<Testcase> findAllTestcasesByUser(Name.User user) {
+        return null;
+    }
 
-    // No Prefixes
-    // A
-    // B
-    public void deleteUser(Name.User user){};
+    // Correct Declarations (choose B)
+    public Account[] accountList;
 
-    // Avoid mental mapping
-    // A
-    // B
+    // No Prefixes (choose B)
+    public void deleteUser(Name.User user) {}
+
+    // Avoid mental mapping (choose B)
     public URL url;
 
-    // Class names should be nouns, functions should have verbs
-    // A
-    public class TableRepresent {};
-    public void saveCar(){};
-    // B
+    // Class names should be nouns, functions should have verbs (choose A)
+    public class TableRepresenter {}
+    public void saveCar() {}
 
-    // One word per concept
-    // A
-    // B
-    public saveUser(){}; createAccount(){}; generateContract(){};
+    // One word per concept (choose A)
+    public void saveUser() {}
+    public void saveAccount() {}
+    public void saveContract() {}
 
-    // Use constants or enums instead of magic numbers or abbreviations
-    // A
-    // B
+    // Use constants instead of magic numbers (choose B)
     public final int DAYS_IN_YEAR = 360;
     public final int HOURS_IN_WEEK = 168;
+
+    // Dummy inner classes to avoid compilation errors
+    public static class User {}
+    public static class Testcase {}
+    public static class Account {}
 }
